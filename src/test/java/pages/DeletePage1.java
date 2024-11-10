b3285bb8660f85cfc0e1e8e8b07a7ad54275b7a7
seleniummmation <seleniummmation@gmail.com>
2024-11-10 19:26:34 +0000

    Delete class created

---
+++ src/test/java/pages/DeletePage1.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class DeletePage1 {
+
+    private By ClickSignInBtn = By.xpath("//a[contains(text(),'Sign in')]");
+    private By AuthenticationPageHeading = By.xpath("//span[contains(text(),'Authentication')]");
+    private By UserEmailField = By.id("email_create");
+    private By CreateUserAccountBtn = By.id("SubmitCreate");
+    private By CreateAccountErrorBox = By.id("create_account_error");
+
+    public By getClickSignInBtn() {
+        return ClickSignInBtn;
+    }
+
+    public void setClickSignInBtn(By clickSignInBtn) {
+        ClickSignInBtn = clickSignInBtn;
+    }
+
+    public By getCreateAccountErrorBox() {
+        return CreateAccountErrorBox;
+    }
+
+    public void setCreateAccountErrorBox(By createAccountErrorBox) {
+        CreateAccountErrorBox = createAccountErrorBox;
+    }
+
+    public By getCreateUserAccountBtn() {
+        return CreateUserAccountBtn;
+    }
+
+    public void setCreateUserAccountBtn(By createUserAccountBtn) {
+        CreateUserAccountBtn = createUserAccountBtn;
+    }
+
+    public By getUserEmailField() {
+        return UserEmailField;
+    }
+
+    public void setUserEmailField(By userEmailField) {
+        UserEmailField = userEmailField;
+    }
+
+    public By getAuthenticationPageHeading() {
+        return AuthenticationPageHeading;
+    }
+
+    public void setAuthenticationPageHeading(By authenticationPageHeading) {
+        AuthenticationPageHeading = authenticationPageHeading;
+    }
+}
