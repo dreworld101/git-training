11cdafdff59fc1c00bc5893340cf8d35f5854692
Drew Omo <drewomo@Drews-MacBook-Pro.local>
2024-11-16 16:24:40 +0000

    Refactor code & add new class

--- src/test/java/pages/NewDeletePage.java
+++ src/test/java/pages/NewDeletePage.java
@@ -7,19 +7,8 @@ public class NewDeletePage {
     private By PrintedDress5099 = By.xpath("//body/div[@id='page']/div[2]/div[1]/div[2]/div[1]/div[1]/ul[1]/li[4]/div[1]/div[2]/h5[1]/a[1]");
     private By AddToCartBtn = By.xpath("//span[contains(text(),'Add to cart')]");
 
-    public By getAddToCartBtn() {
-        return AddToCartBtn;
-    }
-
-    public void setAddToCartBtn(By addToCartBtn) {
-        AddToCartBtn = addToCartBtn;
-    }
-
-    public By getPrintedDress5099() {
-        return PrintedDress5099;
-    }
-
-    public void setPrintedDress5099(By printedDress5099) {
+    public NewDeletePage(By printedDress5099, By addToCartBtn) {
         PrintedDress5099 = printedDress5099;
+        AddToCartBtn = addToCartBtn;
     }
 }
