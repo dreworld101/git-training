aac7ac7b317f23da48b22e726a001d309ba46262
seleniummmation <seleniummmation@gmail.com>
2024-11-16 15:59:54 +0000

    Refactored code

--- src/test/java/pages/GitTraningToBeDeleted.java
+++ src/test/java/pages/GitTraningToBeDeleted.java
@@ -9,6 +9,7 @@ public class GitTraningToBeDeleted {
     private By UserEmailField = By.id("email_create");
     private By CreateUserAccountBtn = By.id("SubmitCreate");
     private By CreateAccountErrorBox = By.id("create_account_error");
+    private By PrintedDress5099 = By.xpath("//body/div[@id='page']/div[2]/div[1]/div[2]/div[1]/div[1]/ul[1]/li[4]/div[1]/div[2]/h5[1]/a[1]");
 
     public void setClickSignInBtn(By clickSignInBtn) {
         ClickSignInBtn = clickSignInBtn;
@@ -28,5 +29,13 @@ public class GitTraningToBeDeleted {
 
     public void setCreateAccountErrorBox(By createAccountErrorBox) {
         CreateAccountErrorBox = createAccountErrorBox;
+    }
+
+    public By getPrintedDress5099() {
+        return PrintedDress5099;
+    }
+
+    public void setPrintedDress5099(By printedDress5099) {
+        PrintedDress5099 = printedDress5099;
     }
 }
