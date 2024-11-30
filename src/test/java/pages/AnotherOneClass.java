c7a7c8227578572b69a140e3b9759be6bebdf4ec
Drew Omo <drewomo@Drews-MacBook-Pro.local>
2024-11-30 16:42:43 +0000

    Refactore AnotherOneClass

--- src/test/java/pages/AnotherOneClass.java
+++ src/test/java/pages/AnotherOneClass.java
@@ -15,58 +15,36 @@ public class AnotherOneClass {
     private By userAccountButton = By.id("SubmitCreate");
 
 
-    public AnotherOneClass(By userEmailValidationField, By enterPassword, By datePicker_Day, By datePicker_Month, By datePicker_Year) {
-        UserEmailValidationField = userEmailValidationField;
-        EnterPassword = enterPassword;
-        DatePicker_Day = datePicker_Day;
-        DatePicker_Month = datePicker_Month;
-        DatePicker_Year = datePicker_Year;
-    }
-
     public By getUserEmailValidationField() {
         return UserEmailValidationField;
     }
 
     public void setUserEmailValidationField(By userEmailValidationField) {
         UserEmailValidationField = userEmailValidationField;
-
-        public By getSignInButton() {
-        return signInButton;
     }
 
     public By getEnterPassword() {
         return EnterPassword;
-
-
+    }
 
     public void setEnterPassword(By enterPassword) {
         EnterPassword = enterPassword;
-    public By getPageHeadingText() {
-        return pageHeadingText;
     }
 
     public By getDatePicker_Day() {
         return DatePicker_Day;
-    public void setPageHeadingText(By pageHeadingText) {
-        this.pageHeadingText = pageHeadingText;
     }
 
     public void setDatePicker_Day(By datePicker_Day) {
         DatePicker_Day = datePicker_Day;
-    public By getEmailFld() {
-        return emailFld;
     }
 
     public By getDatePicker_Month() {
         return DatePicker_Month;
-    public void setEmailFld(By emailFld) {
-        this.emailFld = emailFld;
     }
 
     public void setDatePicker_Month(By datePicker_Month) {
         DatePicker_Month = datePicker_Month;
-    public By getUserAccountButton() {
-        return userAccountButton;
     }
 
     public By getDatePicker_Year() {
@@ -75,6 +53,36 @@ public class AnotherOneClass {
 
     public void setDatePicker_Year(By datePicker_Year) {
         DatePicker_Year = datePicker_Year;
+    }
+
+    public By getSignInButton() {
+        return signInButton;
+    }
+
+    public void setSignInButton(By signInButton) {
+        this.signInButton = signInButton;
+    }
+
+    public By getPageHeadingText() {
+        return pageHeadingText;
+    }
+
+    public void setPageHeadingText(By pageHeadingText) {
+        this.pageHeadingText = pageHeadingText;
+    }
+
+    public By getEmailFld() {
+        return emailFld;
+    }
+
+    public void setEmailFld(By emailFld) {
+        this.emailFld = emailFld;
+    }
+
+    public By getUserAccountButton() {
+        return userAccountButton;
+    }
+
     public void setUserAccountButton(By userAccountButton) {
         this.userAccountButton = userAccountButton;
     }
